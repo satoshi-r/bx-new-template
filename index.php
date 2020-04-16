1d4266e8133a58f89ac994ed80faf32c4d568ee5
satoshi-r <rgeargerge@yandex.ru>
2020-04-16 21:12:46 +0300

    changes

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php $path = "local/templates/main/"; ?>
 <?php include_once 'local/templates/main/header.php'; ?>
 
-<picture>
+<div class="main-page">
+	<picture>
     <source type="image/webp" srcset="<?php echo $path; ?>img/preview.webp">
     <source type="image/jpeg" srcset="<?php echo $path; ?>img/preview.jpg">
     <img src="<?php echo $path; ?>img/preview.jpg" alt="preview">
-</picture>
-<img src="" alt="test">
+  </picture>
+</div>
 
 <?php include_once 'local/templates/main/footer.php'; ?>
